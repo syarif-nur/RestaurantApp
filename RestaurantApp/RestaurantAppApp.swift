4c44c2945f58807528e59cc07324b2f4916e9ea4
Syarif <syarif.nur92@gmail.com>
2024-05-16 08:49:54 +0700

    Initial Commit

---
+++ RestaurantApp/RestaurantAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantAppApp.swift
+//  RestaurantApp
+//
+//  Created by Syarif on 16/05/24.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
